3b73949ec556bc7c4b106d415350cd1b63a46551
kangmin <dididiri1@naver.com>
2017-02-10 14:56:59 +0900

    ff22

--- step05customview/src/main/java/com/gura/step05customview/MyView.java
+++ step05customview/src/main/java/com/gura/step05customview/MyView.java
@@ -1,19 +1,27 @@
 package com.gura.step05customview;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
+/*
+   버튼의 부모는 뷰다
 
-/**
- * Created by user on 2017-02-10.
  */
-
 public class MyView extends View {
+    //생성자1
     public MyView(Context context) {
         super(context);
     }
-
+    //생성자2
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+    // onDraw() 메소드를 재정의한다.
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // View 를 노란색으로 칠하기
+        canvas.drawColor(Color.YELLOW);
     }
 }
