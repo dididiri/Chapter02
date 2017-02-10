2ec3ac5401c61777ab3fbcdde054a3f7e4ba1783
kangmin <dididiri1@naver.com>
2017-02-10 14:46:37 +0900

    ff

---
+++ step05customview/src/main/java/com/gura/step05customview/MyView.java
@@ -0,0 +1,19 @@
+package com.gura.step05customview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by user on 2017-02-10.
+ */
+
+public class MyView extends View {
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
